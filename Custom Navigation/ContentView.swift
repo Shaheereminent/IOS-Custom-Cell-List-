f807f23459532e6d66c55b9f2a64228a404d5a8e
Shaheereminent <shaheerinayatali2@gmail.com>
2022-07-19 21:32:43 +0500

    Initials of custom cell list project

---
+++ Custom Navigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Custom Navigation
+//
+//  Created by Shaheer Inayat Ali on 19/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Custom Navigation/Custom_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_NavigationApp.swift
+//  Custom Navigation
+//
+//  Created by Shaheer Inayat Ali on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Custom_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
